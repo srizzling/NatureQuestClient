684729cbf420b6b4634d14c697fc8c99811728f9
litsoy <litsoy@hotmail.co.nz>
2013-06-25 15:03:30 +1200

    Fixed Radar for my application.

---
+++ src/com/naturequest/radar/GeoPoint.java
@@ -0,0 +1,11 @@
+package com.naturequest.radar;
+
+public class GeoPoint{
+	private int lat;
+	private int longitude;
+	
+	public GeoPoint(int lat, int longtitude){
+		this.lat=lat;
+		this.longitude=longtitude;
+	}
+}
