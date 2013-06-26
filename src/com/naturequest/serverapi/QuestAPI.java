f18760bc909d0c175e07f5d3d0a8962d5b3f11a0
Sriram Venkatesh <venksriram@gmail.com>
2013-06-26 15:13:51 -0400

    Now the server knows who is logged into the client

--- src/com/naturequest/serverapi/QuestAPI.java
+++ src/com/naturequest/serverapi/QuestAPI.java
@@ -3,6 +3,8 @@ package com.naturequest.serverapi;
 import android.util.Log;
 
 import com.loopj.android.http.*;
+import com.naturequest.Game;
+import com.naturequest.User;
 
 public class QuestAPI {
 	
@@ -13,11 +15,23 @@ public class QuestAPI {
 
   public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
 	  Log.d("URL",getAbsoluteUrl(url));
+	  
+	  if(!url.equals("login.json") && !url.equals("signup.json")){
+		  User currUser = Game.getGame().getCurrentUser();		  
+		  params.put("auth_token", currUser.getToken());
+	  }
+	  
       client.get(getAbsoluteUrl(url), params, responseHandler);
   }
 
   public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-      client.post(getAbsoluteUrl(url), params, responseHandler);
+      
+	 
+	  if(!url.equals("login.json") && !url.equals("signup.json")){
+		  User currUser = Game.getGame().getCurrentUser();		  
+		  params.put("auth_token", currUser.getToken());
+	  }
+	  client.post(getAbsoluteUrl(url), params, responseHandler);
   }
 
   private static String getAbsoluteUrl(String relativeUrl) {
